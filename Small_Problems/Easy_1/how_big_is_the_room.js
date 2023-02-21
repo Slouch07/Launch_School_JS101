60549ca28fb2a7b94ed3696cef3b9aaa054a1cdf
Ryan Bowler <ryanbowler@hotmail.com>
2023-02-20 17:52:19 -0800

    installed 'readline-sync' package and code for 'how big is the room' problem.

--- Small_Problems/Easy_1/how_big_is_the_room.js
+++ Small_Problems/Easy_1/how_big_is_the_room.js
@@ -2,3 +2,31 @@
 // and then logs the area of the room to the console in both square meters and square feet.
 
 // Note: 1 square meter == 10.7639 square feet
+
+let rlSync = require('readline-sync');
+
+let roomLength = rlSync.question("What is the length of the room in meters?: ");
+let roomWidth = rlSync.question("What is the width of the room in meters?: ");
+
+let squareMeters = roomLength * roomWidth; 
+let squareFeet = (roomLength * roomWidth) * 10.7369;
+
+console.log(`The area of the room is ${squareMeters} square meters, or ${squareFeet} square feet.`);
+
+// Further Exploration
+
+// let rlSync = require('readline-sync');
+
+// let measurement = rlSync.question("Would you like to calculate the room in meters or feet?: ");
+// let roomLength = rlSync.question("What is the length of the room?: ");
+// let roomWidth = rlSync.question("What is the width of the room?: ");
+
+// if (measurement.toLowerCase() === 'meters') {
+//   let squareMeters = roomLength * roomWidth;
+//   console.log(`The area of the room is ${squareMeters} square meters.`);
+// } else if (measurement.toLowerCase() === 'feet') {
+//   let squareFeet = (roomLength * roomWidth);
+//   console.log(`The area of the room is ${squareFeet} square feet.`);
+// } else {
+//   console.log('Not a valid input.');
+// }
