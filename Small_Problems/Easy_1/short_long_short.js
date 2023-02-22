f9237a9c21118adea419590e70aad9d33b72620a
Ryan Bowler <ryanbowler@hotmail.com>
2023-02-21 19:04:54 -0800

    added code for short_long_short problem

---
+++ Small_Problems/Easy_1/short_long_short.js
@@ -0,0 +1,20 @@
+// Write a function that takes two strings as arguments, determines the length of 
+// the two strings, and then returns the result of concatenating the shorter string,
+// the longer string, and the shorter string once again. You may assume that the strings are of different lengths.
+
+function shortLongShort(strA, strB) {
+  let lengthA = strA.length;
+  let lengthB = strB.length;
+
+  if (lengthA > lengthB) {
+    return strB + strA + strB;
+  } else {
+  return strA + strB + strA;
+  }
+}
+
+
+
+console.log(shortLongShort('abc', 'defgh'));    // "abcdefghabc"
+console.log(shortLongShort('abcde', 'fgh'));    // "fghabcdefgh"
+console.log(shortLongShort('', 'xyz'));         // "xyz"
