d41347a02bcea370ab8a737c5c0186fbaf760686
Ryan Bowler <ryanbowler@hotmail.com>
2023-02-25 06:27:00 -0800

    added completed code for calculator.js

--- Lesson_2/calculator.js
+++ Lesson_2/calculator.js
@@ -12,7 +12,33 @@ console.log('Welcome to Calculator!');
 
 // Ask the user for the first number.
 console.log("What's the first number?")
-// Store user input in a variable.
-let firstNum = readline.question();
 
-console.log(firstNum);
+// Store user input in a variable.
+let num1 = readline.question();
+
+// Ask the user for the second number.
+console.log("What's the second number?")
+
+// Store user input in a variable.
+let num2 = readline.question();
+
+// Ask user which operation to perform.
+console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+
+// Store user input in a variable.
+let operation = readline.question();
+
+// Flow control depending on user's choice of operation.
+let output;
+if (operation === '1') {
+  output = Number(num1) + Number(num2);
+} else if (operation === '2') {
+  output = Number(num1) - Number(num2);
+} else if (operation === '3') {
+  output = Number(num1) * Number(num2);
+} else if (operation === '4') {
+  output = Number(num1) / Number(num2);
+} 
+
+// Log the result to the console.
+console.log(`The result is: ${output}`);
