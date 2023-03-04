27ec2be353c9611d708b65c74577e41540a116c2
Ryan Bowler <ryanbowler@hotmail.com>
2023-03-04 15:51:59 -0800

    Updated calculator.js with some bonus features.

--- Lesson_2/calculator.js
+++ Lesson_2/calculator.js
@@ -1,11 +1,17 @@
-// Ask the user for the first number.
-// Ask the user for the second number.
-// Ask the user for an operation to perform.
-// Perform the operation on the two numbers.
-// Print the result to the terminal.
+// Set default language
+const LANGUAGE = 'en';
+
+// Load JSON config file into the program.
+const MESSAGES = require('./calculator_messages.json');
 
 // Allow input from the user to be stored for use in the program.
 const readline = require('readline-sync');
+
+// A function to select which language to use.
+function messages(message, lang = 'en') {
+  return MESSAGES[lang][message];
+}
+
 
 // A function to give our messages a more distinctive look.
 function prompt(message) {
@@ -24,41 +30,41 @@ let isRunning = true;
 while (isRunning) {
 
   // Greet the user.
-  prompt('Welcome to Calculator!');
+  prompt(messages('welcome', LANGUAGE));
 
   // Ask the user for the first number.
-  prompt("What's the first number?");
+  prompt(messages('firstNumber', LANGUAGE));
 
   // Store user input in a variable.
   let num1 = readline.question();
 
   // Check if the first number is a valid.
   while (invalidNum(num1)) {
-    prompt("Hmm... that doesn't look like a valid number.");
+    prompt(messages('invalidNumber', LANGUAGE));
     num1 = readline.question();
   }
 
   // Ask the user for the second number.
-  prompt("What's the second number?");
+  prompt(messages('secondNumber', LANGUAGE));
 
   // Store user input in a variable.
   let num2 = readline.question();
 
   // Check if the second number is valid.
   while (invalidNum(num2)) {
-    prompt("Hmm... that doesn't look like a valid number.");
+    prompt(messages('invalidNumber', LANGUAGE));
     num2 = readline.question();
   }
 
   // Ask user which operation to perform.
-  prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+  prompt(messages('operation', LANGUAGE));
 
   // Store user input in a variable.
   let operation = readline.question();
 
   // Check that the user input 1, 2, 3, or 4 only.
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt('Please choose 1, 2, 3, or 4');
+    prompt(messages('invalidChoice', LANGUAGE));
     operation = readline.question();
   }
 
@@ -80,9 +86,10 @@ while (isRunning) {
   }
 
   // Log the result to the console.
-  prompt(`The result is: ${output}`);
+  prompt(messages('result', LANGUAGE) + output);
 
-  prompt(`Would you like to perform another calculation? y or n`);
+  // Ask the user if they would like another calculation.
+  prompt(messages('anotherCalc', LANGUAGE));
   let choice = readline.question();
   if (choice[0].toLowerCase() !== 'y') {
     isRunning = false;
