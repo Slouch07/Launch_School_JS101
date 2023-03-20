fb8d8e7411159bd692425fc6488b24550ddfe7d9
Ryan Bowler <ryanbowler@hotmail.com>
2023-03-20 07:09:25 -0700

    added code for "Greeting A User" small problem.

---
+++ Small_Problems/Easy_2/greeting_user.js
@@ -0,0 +1,17 @@
+/*
+Write a program that will ask for user's name.
+The program will then greet the user.
+If the user writes "name!" then the computer yells back to the user.
+*/
+function greeting(name) {
+  if (name.includes('!')) {
+    console.log((`Hi, ${name.slice(0, -1)}. Why are we yelling!!`).toUpperCase());
+  } else {
+    console.log(`Hi, ${name}.`);
+  }
+}
+
+let readline = require('readline-sync');
+let name = readline.question('What is your name?: ');
+
+greeting(name);
