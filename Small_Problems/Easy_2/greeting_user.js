8d0e021f30ab867ae542ff1bd41556ac7ea66ef3
Ryan Bowler <ryanbowler@hotmail.com>
2023-03-20 18:58:30 -0700

    added code for "Multiplying Two Numbers" small problem.

--- Small_Problems/Easy_2/greeting_user.js
+++ Small_Problems/Easy_2/greeting_user.js
@@ -3,6 +3,8 @@ Write a program that will ask for user's name.
 The program will then greet the user.
 If the user writes "name!" then the computer yells back to the user.
 */
+
+
 function greeting(name) {
   if (name.includes('!')) {
     console.log((`Hi, ${name.slice(0, -1)}. Why are we yelling!!`).toUpperCase());
