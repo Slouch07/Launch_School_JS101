70c93c7f5097e243ac232872239130a6a9a31cba
Ryan Bowler <ryanbowler@hotmail.com>
2023-03-22 20:07:27 -0700

    Updated code for 'Exclusive Or'

--- Small_Problems/Easy_2/exclusive_or.js
+++ Small_Problems/Easy_2/exclusive_or.js
@@ -15,3 +15,7 @@ console.log(xor(5, 0) === true);          // true
 console.log(xor(false, true) === true);   // true
 console.log(xor(1, 1) === false);         // true
 console.log(xor(true, true) === false);   // true
+console.log(xor('', 'true') === true);    // true
+console.log(xor('', 0) === false);        // true
+
+
