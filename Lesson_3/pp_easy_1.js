053e8138de9ec98f6744cd7db5ec46d3e4959535
Ryan Bowler <ryanbowler@hotmail.com>
2023-04-08 11:01:17 -0700

    Added code for 'Practice Problems: Easy 1'.

---
+++ Lesson_3/pp_easy_1.js
@@ -0,0 +1,44 @@
+/* eslint-disable max-len */
+
+// Q4: Using the following string, create a new string that contains all lowercase
+// letters except for the first character, which should be capitalized.
+
+// let munstersDescription = "the Munsters are CREEPY and Spooky.";
+
+// function capitalize(string) {
+//   let newString = string.toLowerCase();
+//   return newString[0].toUpperCase() + newString.slice(1);
+// }
+
+// console.log(capitalize(munstersDescription));
+
+// Q6: Add the additional ages to the ages object.
+
+// let ages = { Herman: 32, Lily: 30, Grandpa: 5843, Eddie: 10 };
+// let additionalAges = { Marilyn: 22, Spot: 237 };
+
+// Object.assign(ages, additionalAges);
+
+// console.log(ages);
+
+// Q7: Determine whether the name Dino appears in the strings below -- check each string separately):
+
+// let str1 = "Few things in life are as important as house training your pet dinosaur.";
+// let str2 = "Fred and Wilma have a pet dinosaur named Dino.";
+
+// console.log(str1.includes('Dino'));
+// console.log(str2.includes('Dino'));
+
+// Q9: How can we add multiple items to our array?
+
+// let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+// flintstones.push('Dino', 'Hoppy');
+
+// console.log(flintstones);
+
+// Q10: Return a new version of this sentence that ends just before the word 'house'.
+
+// let advice = "Few things in life are as important as house training your pet dinosaur.";
+
+// console.log(advice.substring(0, advice.indexOf('house')));
+
