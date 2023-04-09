34277f939c69e40742e26f4fcb13b7eff776581f
Ryan Bowler <ryanbowler@hotmail.com>
2023-04-09 16:57:23 -0700

    Added code for 'Practice Problems: Medium 1'.

---
+++ Lesson_3/pp_medium_1.js
@@ -0,0 +1,42 @@
+/* eslint-disable max-len */
+
+// Q1: Write a program that outputs 'The Flintstones Rock!' 10 times,
+//     with each line indented 1 space to the right of the line above it.
+
+// let string = "The Flintstones Rock!";
+// let space = " ";
+
+// for (let count = 0; count < 10; count += 1) {
+//   console.log(space.repeat(count) + string);
+// }
+
+// Q2: Return a new string that swaps the case of all of the letters:
+
+// let munstersDescription = "The Munsters are creepy and spooky.";
+
+// let newMustersDescription = munstersDescription.split("").map(function(char) {
+//   if (char === char.toUpperCase()) {
+//     return char.toLowerCase();
+//   } else {
+//     return char.toUpperCase();
+//   }
+// }).join("");
+
+// console.log(munstersDescription);
+// console.log(newMustersDescription);
+
+// Q3:
+// function factors(number) {
+//   let divisor = number;
+//   let factors = [];
+//   while (divisor > 0) {
+//     if (number % divisor === 0) {
+//       factors.push(number / divisor);
+//     }
+//     divisor -= 1;
+//   }
+//   return factors;
+// }
+
+// console.log(factors(-48));
+
